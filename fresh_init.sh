6fce9394eefaea8e8d5a1f420cba9f132bf7d96b
joanna_Cin <joanna@cinnamon.is>
2020-11-02 04:45:00 +0000

    Restart tmux after installing oh-my-zsh theme

--- fresh_init.sh
+++ fresh_init.sh
@@ -3,4 +3,5 @@ rm -rf ~/.vim ~/.conda ~/miniconda3 ~/.zplug ~/oh-my-zsh
 ln -s ~/dotfiles/vimrc           ~/.vimrc
 ln -s ~/dotfiles/zshrc           ~/.zshrc
 ln -s ~/dotfiles/tmux.conf       ~/.tmux.conf
+tmux kill-server
 zsh
