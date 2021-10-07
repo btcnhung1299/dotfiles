1490b179c4278e022623f66b556b0d7324f84454
Joanna_Cin <joanna@cinnamon.is>
2021-10-07 18:18:26 +0000

    Update init.sh

--- fresh_init.sh
+++
@@ -1,7 +0,0 @@
-rm -f ~/.vimrc ~/.zshrc ~/.tmux.conf
-rm -r ~/.vim ~/.conda ~/miniconda3 ~/.zplug ~/oh-my-zsh
-ln -s ~/dotfiles/vimrc           ~/.vimrc
-ln -s ~/dotfiles/zshrc           ~/.zshrc
-ln -s ~/dotfiles/tmux.conf       ~/.tmux.conf
-echo "exec zsh" >> ~/.bashrc
-cp -r ~/dotfiles/vim/*          ~/.vim
